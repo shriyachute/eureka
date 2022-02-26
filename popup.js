eddfb93234fc5cb46bc0a13bfb9a9858733e8f09
tanyaach <tanyaach@vt.edu>
2022-02-26 13:41:05 -0500

    added code to popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
 
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
