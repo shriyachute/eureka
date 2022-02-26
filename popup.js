e3345cb483d910cf481fdb97c7e1ed51620d7de4
tanyaach <tanyaach@vt.edu>
2022-02-26 14:59:21 -0500

    changed everything

--- popup.js
+++ popup.js
@@ -1,31 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('clickIt');
+  checkPageButton.addEventListener('click', function() {
 
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
-
-
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
+    chrome.tabs.getSelected(null, function(tab) {
+      alert("Hello! Here's your To Do list.");
+    } );
+  }, false);
+}, false);
